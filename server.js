20db0eb6dba2de4a8259ed7fdee41cd8b2f39697
Sumit <sumit500241@gmail.com>
2025-03-09 16:02:28 +0530

    Update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.post("/api/users", (req, res) => {
   });
 });
 
-app.listen(PORT, () =>
-  console.log(`Server running at http://localhost:${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
+});
